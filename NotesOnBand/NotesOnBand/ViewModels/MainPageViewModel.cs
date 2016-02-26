d003e95dace8535a36eb479fc7340ada36a93880
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-02-25 16:58:06 -0800

    Add in the view model for the MainPage with basic class layout.

---
+++ NotesOnBand/NotesOnBand/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesOnBand.ViewModels
+{
+    /// <summary>
+    /// ViewModel for the MainPage (MainPage.xaml)
+    /// </summary>
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        #endregion
+
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+    }
+}
