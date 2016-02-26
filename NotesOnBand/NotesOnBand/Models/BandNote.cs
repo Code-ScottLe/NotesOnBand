c53c9c271b39443a5a1bae8f9143c0668390ffae
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-02-25 17:38:09 -0800

    Done with constructor for the band note

--- NotesOnBand/NotesOnBand/Models/BandNote.cs
+++ NotesOnBand/NotesOnBand/Models/BandNote.cs
@@ -10,7 +10,7 @@ namespace NotesOnBand.Models
     public class BandNote : INotifyPropertyChanged
     {
         #region Fields
-
+        private string note;
 
         #endregion
 
@@ -21,15 +21,47 @@ namespace NotesOnBand.Models
         /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
-
         #endregion
 
         #region Properties
+
+        /// <summary>
+        /// Note that the users have typed to be synced with the Band
+        /// </summary>
+        public string Note
+        {
+            get
+            {
+                return note;
+            }
+
+            set
+            {
+                note = value;
+                OnPropertyChanged("Note");
+            }
+        }
 
         #endregion
 
         #region Constructors
 
+        /// <summary>
+        /// Default Constructor for the BandNote. Create an instance of BandNote with an empty string.
+        /// </summary>
+        public BandNote()
+        {
+            note = string.Empty;
+        }
+
+        /// <summary>
+        /// Constructor for the BandNote. Create an instance of BandNote with the given string
+        /// </summary>
+        /// <param name="customNote">Note to be created with this instance</param>
+        public BandNote(string customNote)
+        {
+            note = customNote;
+        }
         #endregion
 
         #region Methods
