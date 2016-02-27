de06ac5d627e1c2373b291bdfafe5e4de187e54c
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-02-27 04:20:50 -0800

    Try Unit Test.

--- NotesOnBand/NotesOnBandUnitTest/ViewModelsUnitTests/MainPageViewModelUnitTest.cs
+++ NotesOnBand/NotesOnBandUnitTest/ViewModelsUnitTests/MainPageViewModelUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using NotesOnBand.ViewModels;
 
@@ -7,6 +8,18 @@ namespace NotesOnBandUnitTest.ViewModelsUnitTests
     [TestClass]
     public class MainPageViewModelUnitTest
     {
+        [TestMethod]
+        public async Task LoadNotesFromXMLUnitTest()
+        {
+            //Create the instance of the view model
 
+            MainPageViewModel viewModel = new MainPageViewModel();
+
+            //Try to load.
+            await viewModel.LoadNotesFromXML();
+
+            //Test.
+            Assert.AreEqual(viewModel.Note1, "Test Note 1");
+        }
     }
 }
