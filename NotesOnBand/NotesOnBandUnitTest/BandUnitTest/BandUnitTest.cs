ee3dfcf7f62e2abf9cf95cfc29fb145ff7541310
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-02-27 03:11:21 -0800

    Add in UnitTest for the Band class

---
+++ NotesOnBand/NotesOnBandUnitTest/BandUnitTest/BandUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace NotesOnBandUnitTest.BandUnitTest
+{
+    [TestClass]
+    public class BandUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
