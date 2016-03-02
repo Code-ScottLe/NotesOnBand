ff3cd956e8690e36f1fe0e6d742bf362db00ff62
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-03-02 01:07:14 -0800

    Add in the EventArg for the ErrorOccured

---
+++ NotesOnBand/NotesOnBandEngine/Events/ErrorOccurredEventArgs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesOnBandEngine.Events
+{
+    /// <summary>
+    /// Representing the Event Argument for the ErrorOccurred Event
+    /// </summary>
+    public class ErrorOccurredEventArgs : EventArgs
+    {
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Default constructor, hidden.
+        /// </summary>
+        private ErrorOccurredEventArgs()
+        {
+            
+        }
+
+        /// <summary>
+        /// Constructor for the ErrorOccurredEventArgs. To be used with the ErrorOccurred Event
+        /// </summary>
+        /// <param name="message">Error message to pass though</param>
+        public ErrorOccurredEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
