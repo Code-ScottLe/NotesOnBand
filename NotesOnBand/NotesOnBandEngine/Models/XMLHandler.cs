3dba52fe85dd5e99d086477bd1ed4d979a66d2e6
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-04-02 02:23:39 -0700

    Done with CreatenewXML()

--- NotesOnBand/NotesOnBandEngine/Models/XMLHandler.cs
+++ NotesOnBand/NotesOnBandEngine/Models/XMLHandler.cs
@@ -50,7 +50,7 @@ namespace NotesOnBandEngine.Models
         protected XMLHandler()
         {
             syncedNotesXDocument = new XDocument();
-            savedFileName = Windows.ApplicationModel.Package.Current.PublisherDisplayName + Windows.ApplicationModel.Package.Current.DisplayName + Windows.ApplicationModel.Package.Current.Id + ".xml";
+            savedFileName = Windows.ApplicationModel.Package.Current.PublisherDisplayName + Windows.ApplicationModel.Package.Current.DisplayName + Windows.ApplicationModel.Package.Current.Id.Version + ".xml";
         }
 
         #endregion
@@ -77,6 +77,35 @@ namespace NotesOnBandEngine.Models
             throw new NotImplementedException();
         }
 
+
+        /// <summary>
+        /// Create new XDocument that can be saved as new XML for storing notes. 
+        /// </summary>
+        /// <returns></returns>
+        private XDocument CreateNewXML()
+        {
+            XDocument myDocument = new XDocument();
+
+            //Add the root notes.
+            myDocument.Add(new XElement("Notes"));
+
+            //get the root element.
+            var rootElement = myDocument.Descendants().First();
+
+            for(int i = 0; i < 8; i++)
+            {
+                //create a new element
+                XElement noteElement = new XElement("Note");
+                noteElement.SetAttributeValue("index", i);
+                noteElement.SetValue($"Notes #{i+1}");
+
+                //Add that to the root node
+                rootElement.Add(noteElement);
+            }
+
+            //Done adding basics, return it.
+            return myDocument;
+        }
         #endregion
 
     }
