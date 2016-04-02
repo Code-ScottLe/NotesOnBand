90fdb23bfa7a7a6cb8b018d97dfbd35b96fc3e0d
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-04-02 01:41:47 -0700

    Add in the XMLHandler for notes saving.

---
+++ NotesOnBand/NotesOnBandEngine/Models/XMLHandler.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NotesOnBandEngine.Models
+{
+    public class XMLHandler
+    {
+        #region Fields
+        private static XMLHandler currentInstance;
+
+
+        /// <summary>
+        /// The XML Document that hold all the data 
+        /// </summary>
+        private XDocument syncedNotesXDocument;
+
+
+        private string savedFileName;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Get the singleton instance
+        /// </summary>
+        public static XMLHandler Instance
+        {
+            get
+            {
+                if (currentInstance == null)
+                {
+                    currentInstance = new XMLHandler();
+                }
+
+                return currentInstance;
+            }
+        }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor, hidden
+        /// </summary>
+        protected XMLHandler()
+        {
+            syncedNotesXDocument = new XDocument();
+            savedFileName = Windows.ApplicationModel.Package.Current.PublisherDisplayName + Windows.ApplicationModel.Package.Current.DisplayName + Windows.ApplicationModel.Package.Current.Id + ".xml";
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="notes"></param>
+        /// <returns></returns>
+        public async Task SaveToXMLAsync(List<string> notes)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<string>> LoadFromXMLAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
