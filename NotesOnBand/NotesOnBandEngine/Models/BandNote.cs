50c0ead6f6271972e5f7f021ecd77615f9ff5beb
Vu Ngoc Le (Scott Le) <code.scottle@outlook.com>
2016-05-01 19:40:05 -0700

    Implement INotifyPropertyChanged for the BandNote. Title editor is now working correctly

--- NotesOnBand/NotesOnBandEngine/Models/BandNote.cs
+++ NotesOnBand/NotesOnBandEngine/Models/BandNote.cs
@@ -1,14 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NotesOnBandEngine.Models
 {
-    public class BandNote
+    public class BandNote : INotifyPropertyChanged
     {
-        public string Content { get; set; }
-        public string Title { get; set; }
+
+        private string content;
+        private string title;
+
+        public string Content
+        {
+            get
+            {
+                return content;
+            }
+
+            set
+            {
+                content = value;
+                OnPropertyChanged("Content");
+            }
+
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        /// <summary>
+        /// Implement the INotifyPropertyChanged Interface. Use this to notify the View about the property that was changed to perform updates.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        /// <summary>
+        /// Fire up the PropertyChanged event and notify all the listener about the changed property.
+        /// </summary>
+        /// <param name="propertyName">Name of the property that was changed.</param>
+        public void OnPropertyChanged(string propertyName)
+        {
+
+            //Make sure we do have a listener.
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
